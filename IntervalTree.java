0ecff12e36fb1120eed97642247a669b78622954
Grandgentleman <djones39@wisc.edu>
2017-04-11 16:56:58 -0500

    Add files via upload

--- IntervalTree.java
+++ IntervalTree.java
@@ -1,19 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2016 
+// PROJECT:          p4
+// FILE:             IntervalTree.java
+//
+// TEAM:    46 paras
+// Authors: Daniel Jones
+//////////////////////////// 80 columns wide //////////////////////////////////
+
+/**
+ * This class holds 3 objects. An IntervalNode called root, which is the source
+ * node for the tree. Two ints, size, the number of nodes in the tree, 
+ * and height, or how long the longest path of nodes is from root.
+ * 
+ *  This class allows one to access root, size, and height. It also allows one to
+ *  delete a node in the tree, insert one, find if the interval of two nodes
+ *  overlap, find if the tree already contains an interval, and print the stats
+ *  of the tree.
+ *  @author Daniel Jones
+ */
 import java.util.List;
 
 public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T> {
 	
-	// TODO declare any data members needed for this class
+	IntervalNode<T> root;
+	int size;
+	int height;
 
 	@Override
 	public IntervalNode<T> getRoot() {
-		// TODO Auto-generated method stub
-		return null;
+		if(root == null){
+			return null;
+		}
+		return root;
 	}
 
 	@Override
 	public void insert(IntervalADT<T> interval)
 					throws IllegalArgumentException {
-		// TODO Auto-generated method stub
+		
 
 	}
 
@@ -44,17 +68,51 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
+		return size;
 	}
 
 	@Override
 	public int getHeight() {
-		// TODO Auto-generated method stub
+		return height;
 	}
 
 	@Override
 	public boolean contains(IntervalADT<T> interval) {
-		// TODO Auto-generated method stub
+		if(interval.compareTo(root.getInterval()) == 0){
+			return true;
+		}
+		if(interval.compareTo(root.getInterval()) < 0){
+			if(root.getLeftNode() != null){
+				return contains(interval, root.getLeftNode());
+			}
+			return false;
+		}
+		if(interval.compareTo(root.getInterval()) > 0){
+			if(root.getRightNode() != null){
+				return contains(interval, root.getRightNode());
+			}
+			return false;
+		}
+		return false;
+	}
+	
+	private boolean contains(IntervalADT<T> interval, IntervalNode<T> curr){
+		if(interval.compareTo(curr.getInterval()) == 0){
+			return true;
+		}
+		if(interval.compareTo(curr.getInterval()) < 0){
+			if(curr.getLeftNode() != null){
+				return contains(interval, curr.getLeftNode());
+			}
+			return false;
+		}
+		if(interval.compareTo(root.getInterval()) > 0){
+			if(curr.getRightNode() != null){
+				return contains(interval, curr.getRightNode());
+			}
+			return false;
+		}
+		return false;
 	}
 
 	@Override
